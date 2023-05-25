b980072319efc2bd79b5dcce621a9732017774ce
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2023-05-25 12:13:56 -0400

    Cleaned Component of logs.

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -2,12 +2,6 @@ import { For, Show, createEffect } from "solid-js";
 import { limit, offset, setLimit, setOffset } from "../pages/Home";
 
 const Pagination = ({ item }) => {
-  //   createEffect(() => {
-  //     console.log("Offset", offset());
-  //     console.log("Limit", limit());
-  //     console.log("slice", item.slice(offset(), limit()));
-  //     console.log("item", item);
-  //   });
   return (
     <>
       <Show when={offset() >= limit()}>
